87c81a530b96520883c5f89538177499db71d66a
JeickobI <72135997+JeickobI@users.noreply.github.com>
2020-09-30 16:52:38 +0300

    Функция клика и рисования кнопки

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include "TXLib.h"
 
-int main() //interesting fact you gay
+void drawButton(int x, const char* text)
+{
+    txSelectFont("Arial", 30);
+    txRectangle(x, 0, x + 200, 100);
+    txDrawText (x, 0, x + 200, 100, text);
+}
+
+bool cliiiiick(int x)
+{
+        if (txMouseButtons() == 1 &&
+            txMouseX() >= x       &&  txMouseY() >= 0   &&
+            txMouseX() <= x + 200 &&  txMouseY() <= 100)
+            return true;
+        else
+            return false;
+}
+
+
+
+int main()
 {
     txCreateWindow(1280,720);
 
@@ -30,162 +49,52 @@ int main() //interesting fact you gay
 
     while (!GetAsyncKeyState(VK_ESCAPE))
     {
-    txBegin ();
+        txBegin ();
 
-    txClear();
-    txSetColour(TX_BLACK);
-    txRectangle(0, 0, 200, 100);
-    txDrawText (0, 0, 200, 100, "Wheels");
-    txRectangle(200, 0, 400, 100);
-    txDrawText (200, 0, 400, 100, "Engine");
-    txRectangle(400, 0, 600, 100);
-    txDrawText (400, 0, 600, 100, "Hood");
-    txRectangle(600, 0, 800, 100);
-    txDrawText (600, 0, 800, 100, "Roof");
-    Win32::TransparentBlt (txDC(),900,20,200,200,koleso1,0,0,634,634, TX_WHITE);
-    Win32::TransparentBlt (txDC(),900,240,200,200,koleso2,0,0,430,429, TX_WHITE);
-    Win32::TransparentBlt (txDC(),900,480,200,200,koleso3,0,0,900,900, TX_WHITE);
-    Win32::TransparentBlt (txDC(),300,300,200,200,koleso1,0,0,634,634, TX_WHITE);
-
-    if (txMouseButtons() == 1 &&
-        txMouseX() >= 200 &&
-        txMouseX() <= 400 &&
-        txMouseY() >= 0 &&
-        txMouseY() <= 100)
-       {
-         drawKoleso1 = true;
-       }
-
-     if (drawKoleso1)
-         txBitBlt(txDC() , 0, 400, 200, 200, koleso1);
-
-     if (txMouseButtons() == 1 &&
-        txMouseX() >= 200 &&
-        txMouseX() <= 400 &&
-        txMouseY() >= 0 &&
-        txMouseY() <= 100)
-       {
-         drawKoleso2 = true;
-       }
-
-      if (drawKoleso2)
-         txBitBlt(txDC() , 0, 400, 200, 200, koleso2);
-
-     if (txMouseButtons() == 1 &&
-        txMouseX() >= 200 &&
-        txMouseX() <= 400 &&
-        txMouseY() >= 0 &&
-        txMouseY() <= 100)
-       {
-         drawKoleso3 = true;
-       }
-
-      if (drawKoleso3)
-         txBitBlt(txDC() , 0, 400, 200, 200, koleso3);
-
-     if (txMouseButtons() == 1 &&
-        txMouseX() >= 200 &&
-        txMouseX() <= 400 &&
-        txMouseY() >= 0 &&
-        txMouseY() <= 100)
-       {
-         drawCar0 = true;
-       }
-
-       if (drawCar0)
-         txBitBlt(txDC() , 0, 400, 200, 200, car0);
+        txClear();
+        txSetColour(TX_BLACK);
+        //Нопки
+        drawButton(0,   "WHEELS");
+        drawButton(200, "ENGINE");
+        drawButton(400, "HOOD"  );
+        drawButton(600, "ROOF"  );
 
 
-     if (txMouseButtons() == 1 &&
-        txMouseX() >= 200 &&
-        txMouseX() <= 400 &&
-        txMouseY() >= 0 &&
-        txMouseY() <= 100)
-       {
-         drawCar1 = true;
-       }
+
+        //Картинки
+        //Win32::TransparentBlt (txDC(),900,20,200,200,koleso1,0,0,634,634, TX_WHITE);
+        //Win32::TransparentBlt (txDC(),900,240,200,200,koleso2,0,0,430,429, TX_WHITE);
+        //Win32::TransparentBlt (txDC(),900,480,200,200,koleso3,0,0,900,900, TX_WHITE);
+        //Win32::TransparentBlt (txDC(),300,300,200,200,koleso1,0,0,634,634, TX_WHITE);
+
+        //Клики
+        if (cliiiiick(0))
+        {
+            drawKoleso1 = !drawKoleso1;
+            txSleep(200);
+        }
 
 
-       if (drawCar1)
-         txBitBlt(txDC() , 0, 400, 200, 200, car1);
+
+         if (cliiiiick(200))
+           {
+             drawKoleso3 = !drawKoleso3;
+             txSleep(200);
+           }
+
+        if (drawKoleso1)
+        {
+            Win32::TransparentBlt (txDC(),900,20,200,200,koleso1,0,0,634,634, TX_WHITE);
+            Win32::TransparentBlt (txDC(),900,240,200,200,koleso2,0,0,430,429, TX_WHITE);
+        }
 
 
-     if (txMouseButtons() == 1 &&
-        txMouseX() >= 200 &&
-        txMouseX() <= 400 &&
-        txMouseY() >= 0 &&
-        txMouseY() <= 100)
-       {
-         drawCar2 = true;
-       }
-
-      if (drawCar2)
-         txBitBlt(txDC() , 0, 400, 200, 200, car2);
-
-     if (txMouseButtons() == 1 &&
-        txMouseX() >= 200 &&
-        txMouseX() <= 400 &&
-        txMouseY() >= 0 &&
-        txMouseY() <= 100)
-       {
-         drawCar3 = true;
-       }
-
-       if (drawCar3)
-         txBitBlt(txDC() , 0, 400, 200, 200, car3);
-
-    if (txMouseButtons() == 1 &&
-        txMouseX() >= 200 &&
-        txMouseX() <= 400 &&
-        txMouseY() >= 0 &&
-        txMouseY() <= 100)
-       {
-         drawCar4 = true;
-       }
-
-       if (drawCar4)
-         txBitBlt(txDC() , 0, 400, 200, 200, car4);
-
-     if (txMouseButtons() == 1 &&
-        txMouseX() >= 200 &&
-        txMouseX() <= 400 &&
-        txMouseY() >= 0 &&
-        txMouseY() <= 100)
-       {
-         drawCar5 = true;
-       }
-
-      if (drawCar5)
-         txBitBlt(txDC() , 0, 400, 200, 200, car5);
-
-     if (txMouseButtons() == 1 &&
-        txMouseX() >= 200 &&
-        txMouseX() <= 400 &&
-        txMouseY() >= 0 &&
-        txMouseY() <= 100)
-       {
-         drawCar6 = true;
-       }
-
-       if (drawCar6)
-         txBitBlt(txDC() , 0, 400, 200, 200, car6);
+        if (drawKoleso3)
+            txBitBlt(txDC() , 0, 400, 200, 200, koleso3);
 
 
-     if (txMouseButtons() == 1 &&
-        txMouseX() >= 200 &&
-        txMouseX() <= 400 &&
-        txMouseY() >= 0 &&
-        txMouseY() <= 100)
-       {
-         drawCar7 = true;
-       }
-
-       if (drawCar7)
-         txBitBlt(txDC() , 0, 400, 200, 200,car7);
-
-
-     txSleep (1.5);
-     txEnd ();
+         txSleep (1.5);
+         txEnd ();
     }
 
 
