4766758300b500fc9fd38cfd896a3454ec0fbc23
JeickobI <72135997+JeickobI@users.noreply.github.com>
2020-10-03 17:56:20 +0400

    добавил структуру кнопки

--- main.cpp
+++ main.cpp
@@ -1,27 +1,62 @@
 #include "TXLib.h"
 
-void drawButton(int x, const char* text)
+struct Button
 {
-    txSelectFont("Arial", 30);
-    txRectangle(x, 0, x + 200, 100);
-    txDrawText (x, 0, x + 200, 100, text);
-}
+    int x;
+    int y;
+    const char* text;
 
-bool cliiiiick(int x)
+    void draw()
+    {
+        txSelectFont("Comic Sans MS", 30);
+        txRectangle(x, y, x + 200, y + 80);
+        txDrawText (x, y, x + 200, y + 80, text);
+    }
+
+    bool cliiiiick()
+    {
+            if (txMouseButtons() == 1 &&
+                txMouseX() >= x       &&  txMouseY() >= 0   &&
+                txMouseX() <= x + 200 &&  txMouseY() <= 100)
+                return true;
+            else
+                return false;
+    }
+};
+
+
+struct Picture
 {
-        if (txMouseButtons() == 1 &&
-            txMouseX() >= x       &&  txMouseY() >= 0   &&
-            txMouseX() <= x + 200 &&  txMouseY() <= 100)
-            return true;
-        else
-            return false;
+    int x;
+    int y;
+    int width;
+    int height;
+    HDC image;
+};
+
+void drawPicture(Picture pct)
+{
+    Win32::TransparentBlt(txDC(), pct.x, pct.y, 100, 100, pct.image, 0, 0, pct.width, pct.height, TX_WHITE);
 }
-
-
 
 int main()
 {
-    txCreateWindow(1280,720);
+    txCreateWindow(1200,720);
+
+
+    Picture aPictures[10];
+    aPictures[0] = {900, 200, 634, 634, txLoadImage("pic/volkte37.bmp")};
+    aPictures[1] = {900, 400, 430, 429, txLoadImage("pic/kakietodiski.bmp")};
+    aPictures[2] = {900, 600, 900, 900, txLoadImage("pic/bbs.bmp")};
+
+
+    Button btn1 = {0,    0, "ÊÐÛØÀ"};
+    Button btn2 = {200,  0, "ÁÀÌÏÅÐ ÇÀÄ"};
+    Button btn3 = {400,  0, "ÁÀÌÏÅÐ ÏÅÐ"};
+    Button btn4 = {600,  0, "ÊÎË¨ÑÀ"};
+    Button btn5 = {800,  0, "ØÈÏÛ"};
+    Button btn6 = {1000, 0, "ÏÏÏÏÏÏÏÏÏÏ"};
+
 
     HDC koleso1 = txLoadImage("pic/volkte37.bmp");
     bool drawKoleso1 = false;
@@ -29,6 +64,7 @@ int main()
     bool drawKoleso2 = false;
     HDC koleso3 = txLoadImage("pic/bbs.bmp");
     bool drawKoleso3 = false;
+
 
     HDC car0 = txLoadImage ("pic/car0.bmp");
     bool drawCar0 = false;
@@ -53,11 +89,18 @@ int main()
 
         txClear();
         txSetColour(TX_BLACK);
+        btn1.draw();
+        btn2.draw();
+        btn3.draw();
+        btn4.draw();
+        btn5.draw();
+        btn6.draw();
+
         //Íîïêè
-        drawButton(0,   "WHEELS");
-        drawButton(200, "ENGINE");
-        drawButton(400, "HOOD"  );
-        drawButton(600, "ROOF"  );
+        //drawButton(0,   "WHEELS");
+        //drawButton(200, "ENGINE");
+        //drawButton(400, "HOOD"  );
+        //drawButton(600, "ROOF"  );
 
 
 
@@ -68,29 +111,21 @@ int main()
         //Win32::TransparentBlt (txDC(),300,300,200,200,koleso1,0,0,634,634, TX_WHITE);
 
         //Êëèêè
-        if (cliiiiick(0))
+        if (btn1.cliiiiick())
         {
+            //TABLICA
             drawKoleso1 = !drawKoleso1;
             txSleep(200);
         }
 
 
 
-         if (cliiiiick(200))
-           {
-             drawKoleso3 = !drawKoleso3;
-             txSleep(200);
-           }
-
         if (drawKoleso1)
         {
-            Win32::TransparentBlt (txDC(),900,20,200,200,koleso1,0,0,634,634, TX_WHITE);
-            Win32::TransparentBlt (txDC(),900,240,200,200,koleso2,0,0,430,429, TX_WHITE);
+            drawPicture(aPictures[0]);
+            //drawPicture(aPictures[1]);
+            //drawPicture(aPictures[2]);
         }
-
-
-        if (drawKoleso3)
-            txBitBlt(txDC() , 0, 400, 200, 200, koleso3);
 
 
          txSleep (1.5);
